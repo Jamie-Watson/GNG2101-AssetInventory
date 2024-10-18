21ee0b89814aa535a00e56812a4edcb220f24b70
Jamie <jwats018@uottawa.ca>
2024-10-18 01:18:41 -0400

    Add and remove button handlers

--- Web-AssetInventory/ais/src/SearchPage.js
+++ Web-AssetInventory/ais/src/SearchPage.js
@@ -4,17 +4,45 @@ import Navbar from "./Navbar";
 import "./SearchPage.css"
 import SearchPageItemBar from './SearchPageItemBar';
 import SearchBox from './SearchBox';
+import { useState } from 'react';
 
 export default function SearchPage({username}){
+
+    const [isRemove, setRemove]= useState(false);
+    const [isAdd, setAdd]= useState(false);
+    
+    const handleRemove = () => {
+        setRemove(!isRemove);
+    };
+
+    const handleAdd = () => {
+        setAdd(!isAdd);
+    };
     return(
         <div className="container searchPageContainer mb-5">
              <Navbar username={username}/>
              <div className="container blueContainer">
                 <div className="row">
-                    <SearchPageItemBar/>
+                    <SearchPageItemBar handleAdd={handleAdd} handleRemove={handleRemove}/>
                 </div>
                 
-                <SearchBox/>
+                {!isRemove && !isAdd ? (
+                    <SearchBox/>
+                ) : (
+                    <></>
+                )}
+
+                {!isRemove && !isAdd ? (
+                    <SearchBox/>
+                ) : (
+                    <></>
+                )}
+
+                {!isRemove && !isAdd ? (
+                    <SearchBox/>
+                ) : (
+                    <></>
+                )}
                 
              </div>
         </div>
