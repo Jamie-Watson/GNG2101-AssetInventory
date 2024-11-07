718a80352e48fb17650f8ccbd95a5e7d54995b03
Jamie-Watson <146368655+Jamie-Watson@users.noreply.github.com>
2024-11-07 16:25:14 -0500

    locking scan page

--- Web-AssetInventory/ais/src/ScanItemPage.js
+++ Web-AssetInventory/ais/src/ScanItemPage.js
@@ -1,12 +1,12 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from "./Navbar";
+import ScanPageNavbar from "./ScanPageNavbar";
 import "./SearchPage.css";
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 
-export default function ScanItemPage({username, handleSignOut, handleSearchPage, handleScanPage}){
+export default function ScanItemPage({handleSignOut}){
 
 
     //states for saving barcode
@@ -55,7 +55,7 @@ export default function ScanItemPage({username, handleSignOut, handleSearchPage,
 
     return(
         <div className="container searchPageContainer mb-5">
-             <Navbar username={username} handleSignOut={handleSignOut} handleSearch={handleSearchPage} handleScanPage={handleScanPage}/>
+             <ScanPageNavbar handleSignOut={handleSignOut}/>
              <div className="container blueContainer">
                 <div className="row">
                     SCANNNNN
